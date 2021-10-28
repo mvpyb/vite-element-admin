24b2eee83edbb0bf2e15dec8a6547f0026446fb8
mvpyb <454539387@qq.com>
2021-10-28 11:58:25 +0800

    新增potree组件

---
+++ dist/assets/demo.6865bdc9.js
@@ -0,0 +1,1 @@
+import{h as t}from"./index.d3348f6d.js";function e(e){return t.request({method:"get",url:"/demo/tableList",data:e})}export{e as t};
--- dist/assets/demo.ea23f211.js
+++
@@ -1,1 +0,0 @@
-import{h as t}from"./index.b8c13be6.js";function e(e){return t.request({method:"get",url:"/demo/tableList",data:e})}export{e as t};
