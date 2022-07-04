d89cac2eefad292dd2e703902cd4baa7ec0d1ddb
mvpyb <454539387@qq.com>
2022-07-04 09:34:24 +0800

    update

---
+++ dist/assets/demo.de073f9f.js
@@ -0,0 +1,1 @@
+import{h as t}from"./index.d42c3bed.js";function e(e){return t.request({method:"get",url:"/demo/tableList",data:e})}export{e as t};
--- dist/assets/upload.d8a0ab96.js
+++
@@ -1,1 +0,0 @@
-import{h as t}from"./index.7c538c4a.js";function o(o){return t.request({url:"/upload/image",method:"post",data:o})}function e(o){return t.request({url:"/down/blob",method:"post",data:o})}export{e as d,o as u};
