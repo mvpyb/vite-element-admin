cb76d43b2fe2daffa6427848258de31a4a7ea5ea
mvpyb <454539387@qq.com>
2022-07-07 16:13:53 +0800

    Update user.js

--- mock/user.js
+++ mock/user.js
@@ -12,12 +12,34 @@ const loginInfo = () => {
 const userInfo = () => {
   const result = {
     id: 9527,
-    name: '灰是小灰灰的灰',
     avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    name: '灰是小灰灰的灰',
+    phone: '15988888888',
+    email: '454539387@qq.com',
+    identity: '',
     roles: ['admin'],
   }
   return result
-};
+}
+
+const list = () => {
+  const result = []
+  // const total = 10000 * 10
+  const total = 10000 * 10
+  for (let i = 1; i <= total; i++) {
+    const item = {
+      id: '@increment',
+      method : i % 4,
+      deviceType : '@pick(["web", "glass", "mobile", "desktop", "others"])', // 设备名称
+      deviceOs : '@pick(["win10", "win11", "ios", "android", "others"])', // 系统
+      loginAddr : '@city(true)', // 登录地点
+      loginTime :' @datetime()', // 最后登录时间
+    }
+    result.push( item );
+  }
+  
+  return result
+}
 
 export default [
   {
@@ -76,5 +98,17 @@ export default [
         message: 'success'
       }
     },
-  }
+  },
+  
+  {
+    url: '/api/login/history',
+    type: 'get',
+    response: config => {
+      return {
+        code: 200,
+        message: 'success',
+        data: list()
+      }
+    }
+  },
 ]
