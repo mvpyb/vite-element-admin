6694a0dfc86f821b5e741943133e497e27e36df7
mvpyb <454539387@qq.com>
2022-07-08 13:20:45 +0800

    Update prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -3,7 +3,7 @@ module.exports = {
   printWidth: 120, // 每行代码长度（默认80）
   tabWidth: 2, // 每个tab相当于多少个空格（默认2）
   useTabs: false, // 是否使用tab进行缩进（默认false）
-  singleQuote: false, // 使用单引号（默认false）
+  singleQuote: true, // 使用单引号（默认false）
   semi: false, // 声明结尾使用分号(默认true)
   trailingComma: 'es5', // 多行使用拖尾逗号（默认none）
   bracketSpacing: true, // 对象字面量的大括号间使用空格（默认true）
