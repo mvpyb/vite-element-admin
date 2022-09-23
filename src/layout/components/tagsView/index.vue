ffebad5c047334bf4c8b093eef3a286bf1b7e18d
灰是小灰灰的灰 <454539387@qq.com>
2022-09-23 18:02:37 +0800

    update tags view style

--- src/layout/components/tagsView/index.vue
+++ src/layout/components/tagsView/index.vue
@@ -381,7 +381,7 @@ defineOptions( {
     .el-icon-close {
       width: 16px;
       height: 16px;
-      vertical-align: 2px;
+      vertical-align: -2px;
       border-radius: 50%;
       text-align: center;
       transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
